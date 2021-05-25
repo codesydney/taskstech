a2ddad015ed460fd724acb646b5c2b713b3ff230
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-05-25 14:17:39 +1000

    fix minor spelling error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import CustomerSignUp from './components/Forms/CustomerSignUp';
 import CustomerLogin from "./components/Forms/CustomerLogIn";
-import TradieLogin from './components/Forms/TradieLogIn';
+import TradieLogin from './components/Forms/TradieLogin';
 import TradieSignUp from "./components/Forms/TradiesSignUp";
 import CreateNewCustomer from './components/Forms/CreateNewCustomer';
 

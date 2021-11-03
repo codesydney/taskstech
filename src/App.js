ea8520d43a6eda7872d81302cb5bed76f7c1259e
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-11-03 23:20:47 +1100

    Add CustomerRegistration Page
    Add
    -CustomerRegistration Form in component
    -CustomerRegistration in Page
    -customerRegistration function in actions

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import TradieProfileForm from "./components/Trader/TradieProfileForm";
 import JobDiary from "./components/Jobs/JobDiary/JobDiary";
 import InventoryDetail from "./components/inventory/InventoryDetail";
 
+
 import * as Pages from "../src/pages/Pages"
 
 function App() {
@@ -25,6 +26,8 @@ function App() {
         <Route exact path='/view/jobs' component={ViewJobs} />
         <Route exact path='/create/job' component={CreateJob} />
         <Route exact path='/signup' component={Pages.SignUp} />
+        <Route exact path='/create/customer' component={Pages.CustomerRegistration} />
+
 
                 <Auth>
                     <Route exact path="/create/job" component={CreateJob} />

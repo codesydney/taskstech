796310b6f3849880d73ecb515355dc65f1e668e5
ettienekorayyi <corralstephenmelben@gmail.com>
2022-05-27 08:32:51 +1000

    Added New Job button in ViewJobs component.

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,24 +1,27 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { listenAuthState } from '../actions/action';
 import { getIsSignedIn } from '../selector/userSelector';
 
-const Auth = ({children}) => {
+const Auth = ({ children }, history) => {
     const dispatch = useDispatch();
-    const selector = useSelector((state) =>state);
+    const selector = useSelector((state) => state);
     const isSignedIn = getIsSignedIn(selector);
 
     useEffect(() => {
-        if(!isSignedIn){
-            dispatch(listenAuthState())
-        }
+        if (!isSignedIn) dispatch(listenAuthState())
     }, []);
 
-    if(!isSignedIn){
-        return <></>
-    } else {
-        return children
-    }
-    
+    // Adds the custom history object in the children.
+    const childrenWithProps = React.Children.map(children, child => {
+        if (React.isValidElement(child)) {
+            return React.cloneElement(child, {history});
+        }
+        return child;
+    });
+
+    if (!isSignedIn) return <></>
+    else return childrenWithProps 
+
 }
 export default Auth;

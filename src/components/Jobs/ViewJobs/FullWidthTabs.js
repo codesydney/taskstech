e0326318de9597d2fab7c8a1dae5daaf69819d6a
ettienekorayyi <corralstephenmelben@gmail.com>
2022-12-19 08:35:22 +1100

    Removed makeStyle from FullWidthTabs

--- src/components/Jobs/ViewJobs/FullWidthTabs.js
+++ src/components/Jobs/ViewJobs/FullWidthTabs.js
@@ -1,7 +1,6 @@
 import React, {  useEffect,useState } from 'react'; 
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import { makeStyles } from '@mui/styles'; // @mui/styles | @material-ui/core/styles
 import { useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
@@ -10,31 +9,13 @@ import { TabPanel, a11yProps } from '../../../common/TabMenu/TabStyles';
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useDispatch,useSelector } from 'react-redux';  
 import { getAllJobs } from '../../../actions/action';
-
 import DataTable from './DataTable';
 import ControlledAccordions from './ControlledAccordions';
 import SearchBar from '../../../common/SearchBar';
 import SimpleBackdrop from '../../Loading/SimpleBackdrop';
 
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: '60vw',
-  },
-  title: {
-    color: "#1a1a1a",
-    fontFamily: "Comfortaa",
-    textAlign: 'center',
-    marginTop: '1rem',
-    marginBottom: '1rem',
-    fontSize: '32px !important',
-    fontWeight: '700'
-  }
-}));
-
-
 export const FullWidthTabs = ({ history, callback }) => {
-  const classes = useStyles();
   const theme = useTheme();
   const [value, setValue] = useState(0);
   const [searchInitiated, setSearchInitiated] = useState(false);
@@ -60,9 +41,9 @@ export const FullWidthTabs = ({ history, callback }) => {
 
 
   return (
-    <Container maxWidth="lg" className={classes.root}>
+    <Container maxWidth="lg" >
       <header>
-        <p className={`${classes.title}`}>Jobs</p>
+        <p className='title'>Jobs</p>
         <SearchBar setSearchInitiated={setSearchInitiated} />
         <Button
           type='submit'
@@ -95,7 +76,6 @@ export const FullWidthTabs = ({ history, callback }) => {
           <Tab label="Active" {...a11yProps(2)} />
         </Tabs>
       </AppBar>
-
 
       <TabPanel value={value} index={0} dir={theme.direction} >
         {
@@ -138,11 +118,9 @@ export const FullWidthTabs = ({ history, callback }) => {
             : <ControlledAccordions
               jobs={activeJobs}
               parentCallback={callback}
-
             />
         }
       </TabPanel>
-
       <SimpleBackdrop loading={jobs?.loading} />
     </Container>
   );
